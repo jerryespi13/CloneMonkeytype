9c562c51b29e71e1497bbd0bca6d6e0c09d8898b
jerryespi13 <jerryespi@gmail.com>
2024-04-04 16:38:56 -0600

    cambiando color de la letra en dependencia de si se escribió bien o mal

--- app.js
+++ app.js
@@ -26,8 +26,6 @@ function initGame(){
 
     $paragraph.innerHTML = words.map((word, index) => {
         const letters = word.split('')
-        console.log(letters)
-        
         return `<x-word>
             ${letters
                 .map(letter => `<x-letter>${letter}</x-letter>`)
@@ -54,6 +52,49 @@ function initEvent(){
                 }
             }, 1000)
         }
+    })
+
+    $input.addEventListener('keydown', onKeyDown)
+    $input.addEventListener('keyup', onKeyUp)
+}
+
+function onKeyDown(){
+
+}
+
+function onKeyUp(){
+    // recuperamos los elementos actuales
+    const $currentWord = $paragraph.querySelector('x-word.active')
+    const $currentLetter = $paragraph.querySelector('x-letter.active')
+
+    const currentWord = $currentWord.innerText.trim()
+    $input.maxLength = currentWord.length
+
+    // obtenemos todas las letras de la palabra a escribir
+    const $allLetters = $currentWord.querySelectorAll('x-letter')
+  
+
+    // quitamos las clases correct e incorrect
+    $allLetters.forEach($letter => $letter.classList.remove('correct', 'incorrect'))
+
+    // obtenemos lo que el usuario a escrito y comparamos letra a letra
+    $input.value.split('').forEach((char,index) =>{
+        // letra a cambiar estilo en dependencia de si 
+        // la escribe bien o mal
+        const $letter = $allLetters[index]
+
+        // letra a comparar
+        const letterToCheck = currentWord[index]
+
+        // char es la letra escrita por el usuario
+        // aqui se comparan las letras
+        const isCorrect = char === letterToCheck
+
+        // si coinciden se le agrega la clase correct a la letra en el DOM
+        // si no, se le agrega incorrect
+        const letterClass = isCorrect ? 'correct' : 'incorrect'
+        $letter.classList.add(letterClass)
+
     })
 }
 
